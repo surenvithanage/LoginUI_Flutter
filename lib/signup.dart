20a2d1f8d8b4e93b7eb9423404a6d97e8a2124ca
suren <surenanthonyvithanage@gmail.com>
2020-03-01 11:42:52 +0530

    Initial commit

---
+++ lib/signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignupAppPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupAppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
